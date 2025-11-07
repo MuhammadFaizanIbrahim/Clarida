fe08f68d0c014e30bf9d7b630ba5a4886179e023
MuhammadFaizanIbrahim <mfaizanibrahim26@gmail.com>
2025-11-07 17:03:17 +0500

    Initial commit - Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="w-full min-h-screen flex items-center justify-center bg-green-900 text-white text-4xl font-bold">
+      Its Clarida Project
+    </div>
+  );
+}
+
+export default App
