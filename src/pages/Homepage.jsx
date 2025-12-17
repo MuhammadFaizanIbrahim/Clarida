4c9b215764e4076630e42639f1b29055038b1e60
MuhammadFaizanIbrahim <mfaizanibrahim26@gmail.com>
2025-12-18 01:26:43 +0500

    Transition Effect Work for Sections 1-4

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,28 +1,10 @@
-import ActivationTimeline from "../sections/ActivationTimeline";
-import ClaridaDifference from "../sections/ClaridaDifference";
-import GlobalCommunityImpact from "../sections/GlobalCommunityImpact";
-import Hero from "../sections/Hero";
-import InteractiveRegeneration from "../sections/InteractiveRegeneration";
-import RegenerationTimeline from "../sections/RegenerationTimeline";
-import Testimonials from "../sections/Testimonials";
-import VisionaryGuarantee from "../sections/VisionaryGuarantee";
-import ClaridaDifferenceTestimonialsScroll from "../transitions/ClaridaDifferenceTestimonialsScroll";
-import HeroInteractiveScroll from "../transitions/HeroInteractiveScroll";
+import HomepageSectionBySectionScroll from "../transitions/HomepageSectionBySectionScroll";
 
 
 export default function Homepage() {
   return (
     <>
-      <Hero />
-      <InteractiveRegeneration />
-      <Testimonials />
-      <ClaridaDifference />
-      <RegenerationTimeline />
-      <ActivationTimeline />
-      <VisionaryGuarantee />
-      <GlobalCommunityImpact />
-      {/* <HeroInteractiveScroll /> */}
-      {/* <ClaridaDifferenceTestimonialsScroll /> */}
+     <HomepageSectionBySectionScroll />
     </>
   );
 }
